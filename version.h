b872e47138fa70e454846c483e7af6f1da49f3f8
Andrew Hodel <andrewhodel@gmail.com>
2016-03-20 16:35:19 -0500

    first commit

---
+++ version.h
@@ -0,0 +1,7 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+#define VERSION_DATE "Sun, 20 Mar 2016 16:15:31 -0500"
+#define VERSION_HASH "11b9443"
+#define VERSION_BRANCH "master"
+#define VERSION_REPO "https://github.com/popcornmix/omxplayer.git"
+#endif
